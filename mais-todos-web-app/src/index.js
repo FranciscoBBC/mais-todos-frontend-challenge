161e2cc0bf709159fd1a3ac43483c8dd16963261
francisco castro <francisco.ruf@gmail.com>
2023-06-22 22:03:38 -0300

    feat: create react router config

--- mais-todos-web-app/src/index.js
+++ mais-todos-web-app/src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Home from "./routes/home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// creating routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
