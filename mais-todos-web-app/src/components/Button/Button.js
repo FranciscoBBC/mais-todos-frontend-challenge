7bf52ea30f045532bbf51bc4d72995b965f9ee18
francisco castro <francisco.ruf@gmail.com>
2023-06-24 11:51:32 -0300

    feat: create button component

---
+++ mais-todos-web-app/src/components/Button/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { ButtonWrapper } from "./styles";
+
+const Button = () => <ButtonWrapper>ADICIONAR AO CARRINHO</ButtonWrapper>;
+
+export default Button;
---
+++ mais-todos-web-app/src/components/Button/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ButtonWrapper = styled.button`
+  width: 100%;
+  border: none;
+  background: var(--color-emerald);
+  padding: var(--spacing-2);
+  border-radius: 5px;
+  font-weight: 600;
+  color: var(--color-white);
+  font-size: 16px;
+  box-shadow: 0 2px 10px rgb(0 0 0 / 0.2);
+  cursor: pointer;
+`;
