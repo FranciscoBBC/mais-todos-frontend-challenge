c8b563ef8b5c70c79c4b529714c8f7976b4332e2
francisco castro <francisco.ruf@gmail.com>
2023-06-24 17:41:09 -0300

    feat(button): add children

--- mais-todos-web-app/src/components/Button/Button.js
+++ mais-todos-web-app/src/components/Button/Button.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { ButtonWrapper } from "./styles";
 
-const Button = ({ onClick }) => (
-  <ButtonWrapper onClick={() => onClick()}>ADICIONAR AO CARRINHO</ButtonWrapper>
+const Button = ({ onClick, children }) => (
+  <ButtonWrapper onClick={() => onClick()}>{children}</ButtonWrapper>
 );
 
 export default Button;
