aa14b86aa911a85c2ca6a08f059a4f7127358cbe
francisco castro <francisco.ruf@gmail.com>
2023-06-24 14:47:51 -0300

    feat: add callback function to buttom component

--- mais-todos-web-app/src/components/Button/Button.js
+++ mais-todos-web-app/src/components/Button/Button.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { ButtonWrapper } from "./styles";
 
-const Button = () => <ButtonWrapper>ADICIONAR AO CARRINHO</ButtonWrapper>;
+const Button = ({ onClick }) => (
+  <ButtonWrapper onClick={() => onClick()}>ADICIONAR AO CARRINHO</ButtonWrapper>
+);
 
 export default Button;
